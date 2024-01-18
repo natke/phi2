eb944f5ebbd7faef51ab00b6848ee7cd8d423f49
natke <nakersha@microsoft.com>
2024-01-17 17:23:15 -0800

    Add initial PyTorch scripts

---
+++ run_phi2_opt_onnx.py
@@ -0,0 +1,19 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from optimum.onnxruntime import ORTModelForCausalLM
+
+#torch.set_default_device("cuda")
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
+
+model = ORTModelForCausalLM.from_pretrained("microsoft/phi-2", trust_remote_code=True, export=True)
+
+inputs = tokenizer('''def print_prime(n):
+   """
+   Print all primes between 1 and n
+   """''', return_tensors="pt", return_attention_mask=False)
+
+outputs = model.generate(**inputs, max_length=200)
+print(outputs.shape)
+text = tokenizer.batch_decode(outputs)[0]
+print(text)
