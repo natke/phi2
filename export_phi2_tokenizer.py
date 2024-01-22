4f20f97899228a071928b4c9dcd7fbfdee06f98c
Nat Kershaw <nakersha@microsoft.com>
2024-01-22 22:41:09 +0000

    Add ONNX tokenization and detokenization

--- export_phi2_tokenizer.py
+++ export_phi2_tokenizer.py
@@ -2,12 +2,8 @@ from transformers import AutoTokenizer
 import onnx
 from onnxruntime_extensions import OrtPyFunction, gen_processing_models
 
-
 # phi-2 tokenizer
 phi2_hf_tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", use_fast=False)
-
-phi2_onnx_model = gen_processing_models(phi2_hf_tokenizer, pre_kwargs={})[0]
-
-#phi2_onnx_model = OrtPyFunction(gen_processing_models(phi2_hf_tokenizer, pre_kwargs={})[0])
-
-onnx.save_model(phi2_onnx_model, "models/microsoft/phi-2/tokenizer.onnx")
+phi2_tokenizer, phi2_detokenizer = gen_processing_models(phi2_hf_tokenizer, pre_kwargs={}, post_kwargs={})
+onnx.save_model(phi2_tokenizer, "models/microsoft/phi-2/tokenizer.onnx")
+onnx.save_model(phi2_detokenizer, "models/microsoft/phi-2/detokenizer.onnx")
--- run_phi2_genai_hf_tokenizer.py
+++ run_phi2_genai_hf_tokenizer.py
@@ -1,8 +1,7 @@
 import argparse
 import numpy as np
 import time
-import onnxruntime
-import onnxruntime_extensions as extensions
+from transformers import AutoTokenizer
 import onnxruntime_genai as og
 
 argparser = argparse.ArgumentParser()
@@ -13,6 +12,9 @@ name = argparser.parse_args().name
 # device_type = og.DeviceType.CPU
 device_type = og.DeviceType.CUDA
 
+# Generate input tokens from the text prompt
+tokenizer = AutoTokenizer.from_pretrained('microsoft/phi-2', trust_remote_code=True)
+
 print("Loading model...")
 model=og.Model(f'models/{name}', device_type)
 print("Model loaded")
@@ -22,18 +24,11 @@ prompt = '''def print_prime(n):
     Print all primes between 1 and n
     """'''
 
-# Create the tokenizer and detokenizer (temporary, until tokenization API is added to extensions)
-options = onnxruntime.SessionOptions()
-options.register_custom_ops_library(extensions.get_library_path())
-
-tokenizer = onnxruntime.InferenceSession(f'models/microsoft/phi-2/tokenizer.onnx', options)
-detokenizer = onnxruntime.InferenceSession(f'models/microsoft/phi-2/detokenizer.onnx', options)
-
-tokens = tokenizer.run(None, { "input_text": np.array([prompt] ) })
+tokens = tokenizer.encode(prompt, return_tensors='np')
 
 params=og.SearchParams(model)
 params.max_length = 200
-params.input_ids = tokens[0]
+params.input_ids = tokens
 
 start_time=time.time()
 output_tokens=model.Generate(params)
@@ -41,8 +36,5 @@ run_time=time.time()-start_time
 
 print(f"Tokens: {len(output_tokens)} Time: {run_time:.2f} Tokens per second: {len(output_tokens)/run_time:.2f}")
 
-output = detokenizer.run(None, { "ids": output_tokens })
-
 print("Output:")
-print(output[0][0])
-
+print(tokenizer.decode(output_tokens))
