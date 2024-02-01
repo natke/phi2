3a958c18a8d5b923e40a7a4e53598d3c919e93d2
Nat Kershaw <nakersha@microsoft.com>
2024-02-01 20:16:24 +0000

    Update to genai tokenizer API

--- python/run_phi2_genai_ort.py
+++ python/run_phi2_genai_ort.py
@@ -1,38 +1,32 @@
 import argparse
 import numpy as np
 import time
-import onnxruntime
-import onnxruntime_extensions as extensions
 import onnxruntime_genai as og
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument('--name', type=str, default='microsoft/phi-2/int4', help='Phi-2 model to run')
+argparser.add_argument('--name', type=str, default='microsoft/phi-2/fp32', help='Phi-2 model to run')
 
 name = argparser.parse_args().name
 
-# device_type = og.DeviceType.CPU
+#device_type = og.DeviceType.CPU
 device_type = og.DeviceType.CUDA
 
+
 print("Loading model...")
-model=og.Model(f'models/{name}', device_type)
+model=og.Model(f'../models/{name}', device_type)
 print("Model loaded")
+
+tokenizer = model.CreateTokenizer()
 
 prompt = '''def print_prime(n):
     """
     Print all primes between 1 and n
     """'''
 
-# Create the tokenizer and detokenizer (temporary, until tokenization API is added to extensions)
-options = onnxruntime.SessionOptions()
-options.register_custom_ops_library(extensions.get_library_path())
-
-tokenizer = onnxruntime.InferenceSession(f'models/microsoft/phi-2/tokenizer.onnx', options)
-detokenizer = onnxruntime.InferenceSession(f'models/microsoft/phi-2/detokenizer.onnx', options)
-
-tokens = tokenizer.run(None, { "input_text": np.array([prompt] ) })
+tokens = tokenizer.encode(prompt)
 
 params=og.SearchParams(model)
-# params.max_length = 200
+params.max_length = 200
 params.input_ids = tokens[0]
 
 start_time=time.time()
@@ -41,8 +35,8 @@ run_time=time.time()-start_time
 
 print(f"Tokens: {len(output_tokens)} Time: {run_time:.2f} Tokens per second: {len(output_tokens)/run_time:.2f}")
 
-output = detokenizer.run(None, { "ids": output_tokens })
+text = tokenizer.batch_decode(output_tokens)[0]
 
 print("Output:")
-print(output[0][0])
+print(text)
 
